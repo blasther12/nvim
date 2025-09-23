9a27ce30d1a0bbd22e80f50b9757fe96ee9d3dc5
Rafael Oliveira <37752682+blasther12@users.noreply.github.com>
2025-09-23 14:40:46 -0300

    Update conform.lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -6,12 +6,25 @@ return {
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff_format', 'ruff_organize_imports', 'ruff_fix' },
+        python = { 'ruff_organize_imports', 'ruff_fix' }, -- ou {'ruff_format', ...}
         go = { 'goimports' },
         bash = { 'shfmt' },
+
+        -- adicionados
+        javascript = { 'prettierd', 'eslint_d' }, -- tenta prettier, fallback eslint_d
+        typescript = { 'prettierd', 'eslint_d' },
+        javascriptreact = { 'prettierd', 'eslint_d' },
+        typescriptreact = { 'prettierd', 'eslint_d' },
+        json = { 'prettierd' },
+        jsonc = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        scss = { 'prettierd' },
+        markdown = { 'prettierd' },
+        yaml = { 'prettierd' },
       },
       format_on_save = {
-        -- These options will be passed to conform.format()
+        -- opções repassadas para conform.format()
         timeout_ms = 500,
         lsp_format = 'fallback',
       },
